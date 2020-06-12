fccdfc910f507aa7cf4a58eb93137514ffa39ac9
David Glaser <glaser_david_s@solarturbines.com>
2020-06-12 10:39:49 -0500

    added custom user model

--- README.md
+++ README.md
@@ -9,8 +9,13 @@ https://github.com/LondonAppDeveloper/recipe-app-api
 To run a command in a docker container from the compose
 
     docker-compose run app sh -c "django-admin.py startproject app .
+    
+    docker-compose run app sh -c "python manage.py startapp core"
 
 
 To Run tests and linter
 
     docker-compose run app sh -c "python manage.py test  && flake8
+
+
+Has custome User model
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """ extend baseuser manager override a couple functions
+    email instead of username
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves the new user"""
+        user = self.model(email=email, **extra_fields)  # same as creating a user model
+        user.set_password(raw_password=password)  # encrypt password
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):  # provide out of the box django user model and permisions
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()  # make a new user manager for our object
+
+    USERNAME_FIELD = 'email'  # change username to use email
+
